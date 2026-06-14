86dc770b6b7285e57a111e2cdb7f35990cb5981a
Stefan Vladu <vladustefan01@gmail.com>
2026-06-14 19:30:00 +0300

    scaffold cleanup, wire routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AlertController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/item/{slug}', [DashboardController::class, 'show'])->name('item.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/alerts', [AlertController::class, 'index'])->name('alerts.index');
+    Route::post('/alerts', [AlertController::class, 'store'])->name('alerts.store');
+    Route::delete('/alerts/{alert}', [AlertController::class, 'destroy'])->name('alerts.destroy');
 });
